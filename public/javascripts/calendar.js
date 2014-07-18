3285ba1495d1df6b13c69be0b2be72280160e26d
gyj <505324327@qq.com>
2014-07-18 22:56:15 +0800

    功能完成，左右滑动查看月份，长按表头跳转日期，可自定义点击日期响应函数。

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -3,33 +3,137 @@
  */
 function Calendar(opt){
 	this._container=document.getElementById(opt.containerId);
+	this._selectDateEvent=opt.selectDate;
 	this.init();
 	this.renderDOM();
 	this.bindDOM();
+	this._selectDateEvent&&this.selectDateEvent();
 }
+Calendar.prototype.selectDateEvent=function(){
+	var that=this;
+	$("body").on("tap","td",function(){
+		var curTime=that.createCaption("getCur");
+		var thisDate = parseInt($(this).html());
+		if($(this).hasClass("notCurMon")){
+			if($(this).hasClass("last")){
+				if(curTime.month-1===0){
+					curTime.month=12;
+					curTime.year-=1;
+				}else{
+					curTime.month-=1;
+				}
 
+			}else if($(this).hasClass("next")){
+				if(curTime.month+1===13){
+					curTime.month=1;
+					curTime.year+=1;
+				}else{
+					curTime.month+=1;
+				}
+			}
+		}
+		curTime.date = thisDate;
+		console.log("curTime :",curTime);
+		that._selectDateEvent.call(undefined,curTime);
+	});
+}
 Calendar.prototype.init=function(){
 	this._calendarWrap = document.createElement("div");
 	this._calendarWrap.id = "calendar";
 	this._table=document.createElement("table");
 	this._caption=document.createElement("caption");
-	//	获取当前日期
-	this.time={};
-	this.time.Date=new Date();
-//	获取当前年份
-	this.time.year=this.time.Date.getFullYear();
-//	获取当前月份 (打印发现当前月份为 7  打印出来为 6)
-	this.time.month=this.time.Date.getMonth()+1;
-//	获取当前日（一个月中）
-	this.time.date=this.time.Date.getDate();
-//	获取当前是星期几
-	this.time.day=this.time.Date.getDay();
 
-	console.log(this.time);
+	this.time={};
+	this.time.Date=new Date(); //	获取当前日期
+	this.time.year=this.time.Date.getFullYear();  //	获取当前年份
+	this.time.month=this.time.Date.getMonth()+1;  //	获取当前月份 (打印发现当前月份为 7  打印出来为 6)
+	this.time.date=this.time.Date.getDate();  //	获取当前日（一个月中）
+	this.time.day=this.time.Date.getDay();  //	获取当前是星期几
+
+
+//	跳转日期组件
+	this._skipObj=document.createElement("div");
+	this._skipObj.id = "skipObj";
+	this._skipObj.className="hide";
+
+	this._skipShade=document.createElement("div");
+	this._skipShade.id = "skip_shade";
+
+	this._skip=document.createElement("div");
+	this._skip.id = "skip";
+
+	this._skipHeader=document.createElement("div");
+	this._skipHeader.className = "skip_header";
+	this._skipHeader.innerHTML="跳转日期";
+
+	this._skipWrap=document.createElement("div");
+	this._skipWrap.className = "skip_wrap";
+
+	this._skipYear=document.createElement("div");
+	this._skipYear.id = "skip_year";
+	this._skipYearUl=document.createElement("ul");
+	this._skipYearUl.id = "skip_year_ul";
+	this._skipYear.appendChild(this._skipYearUl);
+
+	this._skipMonth=document.createElement("div");
+	this._skipMonth.id = "skip_month";
+	this._skipMonthUl=document.createElement("ul");
+	this._skipMonthUl.id = "skip_month_ul";
+	this._skipMonth.appendChild(this._skipMonthUl);
+
+	this._skipSelect=document.createElement("div");
+	this._skipSelect.className = "skip_select";
+	this._skipSelectYear=document.createElement("span");
+	this._skipSelectYear.className="year";
+	this._skipSelectYear.innerHTML="年";
+	this._skipSelectMonth=document.createElement("span");
+	this._skipSelectMonth.className="month";
+	this._skipSelectMonth.innerHTML="月";
+	this._skipSelect.appendChild(this._skipSelectYear);
+	this._skipSelect.appendChild(this._skipSelectMonth);
+
+	this._skipWrap.appendChild(this._skipYear);
+	this._skipWrap.appendChild(this._skipMonth);
+	this._skipWrap.appendChild(this._skipSelect);
+
+	this._skipEvent=document.createElement("div");
+	this._skipEvent.className = "skip_event";
+	this._skipEventCancel=document.createElement("div");
+	this._skipEventCancel.className="skip_cancel";
+	this._skipEventCancel.innerHTML="取消";
+	this._skipEventGo=document.createElement("div");
+	this._skipEventGo.className="skip_go";
+	this._skipEventGo.innerHTML="跳转";
+	this._skipEvent.appendChild(this._skipEventCancel);
+	this._skipEvent.appendChild(this._skipEventGo);
+
+	this._skip.appendChild(this._skipHeader);
+	this._skip.appendChild(this._skipWrap);
+	this._skip.appendChild(this._skipEvent);
+
+	this._skipObj.appendChild(this._skipShade);
+	this._skipObj.appendChild(this._skip);
+	this._calendarWrap.appendChild(this._skipObj);
+
+	fillSkipUl(this.time.year,this._skipYearUl);
+	fillSkipUl(this.time.month,this._skipMonthUl);
 };
+
+function fillSkipUl(time,obj){
+	obj.innerHTML="";
+	for(var i = time-2;i<=time+2;i++){
+		var skipYearLi=document.createElement("li");
+		skipYearLi.innerHTML=i;
+		obj.appendChild(skipYearLi);
+	}
+	$(obj).children().eq(2).addClass("selected");
+}
 
 function getDaysOfMonth(date){
 	return new Date(date.getFullYear(), (date.getMonth()+1), 0).getDate();
+}
+function generateMonObj(opt){
+	return new Date(opt.month+",1,"+opt.year);
 }
 function getMonAttr(opt){
 //	构造月份的date对象
@@ -109,7 +213,7 @@ Calendar.prototype.createMonContent=function(opt){
 
 
 	for(var i = opt.curObj.dayOfMonBegin-1;i>=0;i--){
-		 tmpContentString="<td class='notCurMon'>"+(opt.lastObj.daysOfMon-i)+"</td>";
+		 tmpContentString="<td class='notCurMon last'>"+(opt.lastObj.daysOfMon-i)+"</td>";
 		 ContentString+=tmpContentString;
 		weekIndex+=1;
 	}
@@ -135,7 +239,7 @@ Calendar.prototype.createMonContent=function(opt){
 //	如果月末没填满最后一行，就用下个月的来填
 	if((opt.curObj.dayOfMonEnd+1)%7!==0){
 		for(var k = 1;weekIndex%7!==0;k+=1){
-			tmpContentString="<td class='notCurMon'>"+k+"</td>";
+			tmpContentString="<td class='notCurMon next'>"+k+"</td>";
 			ContentString+=tmpContentString;
 			weekIndex+=1;
 			if(weekIndex%7===0){
@@ -149,7 +253,6 @@ Calendar.prototype.createMonContent=function(opt){
 };
 //生成表头 显示当前正查看的年份和月份
 Calendar.prototype.createCaption=function(opt){
-	console.log(opt);
 //	获取要显示的年份
 	var year="";
 //	获取要显示的月份
@@ -175,6 +278,11 @@ Calendar.prototype.createCaption=function(opt){
 				year=curYear;
 				month=parseInt(curMonth)+1;
 			}
+		}else if(opt === "getCur"){
+			return{
+				year:parseInt(curYear),
+				month:parseInt(curMonth)
+			}
 		}
 	}else{
 		year=opt.getFullYear();
@@ -182,8 +290,6 @@ Calendar.prototype.createCaption=function(opt){
 		month+=1;
 	}
 
-	console.log("year:",year);
-	console.log("month:",month);
 	this._caption.innerHTML="<span id='curYear'>"+year+"</span> 年 " +
 		"<span id='curMonth'>"+month+"</span> 月";
 };
@@ -230,7 +336,23 @@ Calendar.prototype.renderDOM=function(){
 	this.createMonBody();
 };
 Calendar.prototype.bindDOM=function(){
-	var that=this;
+	var that=this,
+//		跳转日期变量
+		startY="",
+		pathY="",
+		offsetY="",
+		endY="",
+		liY=30,
+		startHideCount= 0,
+		year=this._skipYear,
+		yearUl=this._skipYearUl,
+		month=this._skipMonth,
+		monthUl=this._skipMonthUl,
+		eventCancel=this._skipEventCancel,
+		eventGo=this._skipEventGo,
+		tmpCreateTop= 0,
+		tmpCreateBottom=0;
+
 	$(".curMon").show();
 	$("body").on("swipeRight","tbody",function(){
 		var $lastMon= $(".lastMon");
@@ -260,19 +382,181 @@ Calendar.prototype.bindDOM=function(){
 		$(this).hide();
         $nextMon.show();
         $nextMon[0].addEventListener("webkitAnimationEnd",function(){
-        $nextMon.removeClass("animated fadeInRight");
-        $nextMon[0].removeEventListener("webkitAnimationEnd")
+	        $nextMon.removeClass("animated fadeInRight");
+	        $nextMon[0].removeEventListener("webkitAnimationEnd")
 	    },false);
-      $(".lastMon").remove();
-      $(".curMon").addClass("lastMon").removeClass("curMon");
-      $nextMon.addClass("curMon").removeClass("nextMon");
-      that.createCaption("next");
-      that.monthes=getMonthes();
-      var nextMon ={
-          curObj : that.monthes.next,
-          lastObj : that.monthes.cur,
-          classObj:"monBlock nextMon"
-      };
-      that.createMonContent(nextMon);
-	})
+	    $(".lastMon").remove();
+	    $(".curMon").addClass("lastMon").removeClass("curMon");
+	    $nextMon.addClass("curMon").removeClass("nextMon");
+	    that.createCaption("next");
+	    that.monthes=getMonthes();
+	    var nextMon ={
+	        curObj : that.monthes.next,
+	        lastObj : that.monthes.cur,
+	        classObj:"monBlock nextMon"
+	    };
+	    that.createMonContent(nextMon);
+	});
+
+
+//  跳转日期事件绑定
+	function touchstartHandler(e){
+		startY=e.touches[0].pageY;
+		if(startY===""){
+			startY+=startHideCount*liY;
+		}
+		tmpCreateTop=0;
+		tmpCreateBottom=0;
+	}
+	function touchmoveHandler(e,obj,type){
+		e.preventDefault();
+		pathY=e.touches[0].pageY;
+		offsetY=pathY-startY;
+//		touchmove中将动态创建的li 起始为0；向下滑动超过30px则创建元素 创建li个数为offsetY/30
+		if(offsetY>0){
+			while(tmpCreateTop<offsetY/30){
+				var nowFirstLiContent=$(obj).children().first().html(),
+					tmpli=document.createElement("li");
+				if(type==="month"&&parseInt(nowFirstLiContent)-1===0){
+					tmpli.innerHTML=12;
+				}else{
+					tmpli.innerHTML=parseInt(nowFirstLiContent)-1;
+				}
+				startHideCount+=1;
+				tmpli.style.cssText="-webkit-transform:translate3d(0,"+(offsetY+startHideCount*liY)+"px,0)";
+				obj.insertBefore(tmpli, obj.firstChild);
+				tmpCreateTop+=1;
+				$(obj).children().last().remove();
+			}
+		}else{
+			while(tmpCreateBottom<(-offsetY/45)){
+				var nowLastLiContent=$(obj).children().last().html(),
+					tmpli=document.createElement("li");
+				if(type==="month"&&parseInt(nowLastLiContent)+1===13){
+					tmpli.innerHTML=1;
+				}else{
+					tmpli.innerHTML=parseInt(nowLastLiContent)+1;
+				}
+				startHideCount-=1;
+				tmpli.style.cssText="-webkit-transform:translate3d(0,"+(offsetY+startHideCount*liY)+"px,0)";
+				obj.appendChild(tmpli);
+				tmpCreateBottom+=1;
+				$(obj).children().first().remove();
+			}
+		}
+		var lis=$(obj).children();
+		for(var i = 0,len=lis.length;i<len;i++){
+			lis.eq(i)[0].style.cssText="-webkit-transform:translate3d(0,"+(offsetY+(tmpCreateBottom-tmpCreateTop)*liY)+"px,0)";
+		}
+		$(obj).find(".selected").removeClass("selected");
+	}
+	function touchendHandler(e,obj){
+		endY=e.changedTouches[0].pageY;
+		offsetY=endY-startY;
+		$(obj).children().eq(2).addClass("selected");
+		var lis=$(obj).children();
+		for(var i = 0,len=lis.length;i<len;i++){
+			lis.eq(i)[0].style.cssText="-webkit-transform:translate3d(0,0,0)";
+		}
+	}
+	addListener(year, "touchstart",function(e){
+		touchstartHandler(e);
+	});
+	addListener(year, "touchmove",function(e){
+		touchmoveHandler(e, yearUl,"year");
+	});
+	addListener(year, "touchend",function(e){
+		touchendHandler(e,yearUl);
+	});
+	addListener(month, "touchstart",function(e){
+		touchstartHandler(e);
+	});
+	addListener(month, "touchmove",function(e){
+		touchmoveHandler(e, monthUl,"month");
+	});
+	addListener(month, "touchend",function(e){
+		touchendHandler(e,monthUl);
+	});
+
+	addListener(eventCancel, "touchstart",function(e){
+		$(eventCancel).addClass("yes");
+	});
+	addListener(eventCancel, "touchend",function(e){
+		$(eventCancel).removeClass("yes");
+		that._skipObj.className="hide";
+	});
+
+	addListener(eventGo, "touchstart",function(e){
+		$(eventGo).addClass("yes");
+	});
+	addListener(eventGo, "touchend",function(e){
+		$(eventGo).removeClass("yes");
+		var skipObj={
+			year : parseInt($("#skip_year_ul>.selected").html()),
+			month: parseInt($("#skip_month_ul>.selected").html())
+		}
+		var skipTo=generateMonObj(skipObj);
+		that.createCaption(skipTo);
+		that.monthes=getMonthes();
+		$(".monBlock").remove();
+		that.createMonBody();
+		$(".curMon").show();
+//		渐隐
+		fade(that._skipObj,"hiden",function(){
+			that._skipObj.className="hide";
+			that._skipObj.style.opacity=1;
+		});
+	});
+
+	$(that._caption).on("longTap",function(){
+		that._skipObj.className="";
+		var curTime=that.createCaption("getCur");
+		fillSkipUl(parseInt(curTime.year),that._skipYearUl);
+		fillSkipUl(parseInt(curTime.month),that._skipMonthUl);
+	});
 };
+
+function addListener(target, type, handler){
+	if(target.addEventListener){
+		target.addEventListener(type, handler, false);
+	}else if(target.attachEvent){
+		target.attachEvent("on" + type, handler);
+	}else{
+		target["on" + type] = handler;
+	}
+}
+
+//渐显渐隐函数
+function fade(obj,method,callback){ //method有两个值show或hiden callback函数为渐显渐隐结束后的回调函数，若无可省
+	var n = (method == "show") ? 0 : 100,
+		ie = (window.ActiveXObject) ? true : false;
+	var time = setInterval(function(){
+		if(method == "show"){
+			if(n < 100){
+				n+=10;
+				if(ie){
+					obj.style.cssText = "filter:alpha(opacity="+n+")";
+				}else{
+					(n==100) ? obj.style.opacity = 1 : obj.style.opacity = "0."+n;
+				}
+			}else{
+				clearTimeout(time);
+				callback&&callback();
+			}
+		}else{
+			if(n > 0){
+				n-=10;
+				if(ie){
+					obj.style.cssText = "filter:alpha(opacity="+n+")";
+				}else{
+					obj.style.opacity = "0."+n;
+				}
+			}else{
+				clearTimeout(time);
+				callback&&callback();
+			}
+		}
+	},30);
+}
+
+
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,12 @@
  * Created by Administrator on 14-7-15.
  */
 new Calendar({
-	containerId:"container"
+	containerId:"container",
+	selectDate:function(data){
+		var year=data.year;
+		var month=data.month;
+		var date=data.date;
+		alert(year+" 年 "+month+" 月 "+date+" 日");
+	}
 });
 
